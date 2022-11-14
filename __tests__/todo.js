e4f3c7e19f2e561c50ca1f11b133a43ed18a9a61
rwandantechy <niyinnocent2027@.com>
2022-11-15 00:04:23 +0530

    new folder and files

---
+++ __tests__/todo.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+const todoList = require('../index');
+// eslint-disable-next-line no-unused-vars
+const { all,add,markAsComplete,overdue,dueToday,dueLater,toDisplayableList} = todoList();
+
+describe("todolist test suite", () => {
+    beforeAll(() => {
+        add({
+            title: "Test todo",
+            completed: false,
+            dueDate: new Date().toLocaleDateString("en-CA")
+        });
+    
+});
+
+        test("Should add new todo ", () => {
+            const todoItemCount = all.length;
+            add({
+                title: "Test todo",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA")
+            });
+    
+            expect(all.length).toBe(todoItemCount+1);
+        });
+
+        test("Should mark todo  as complete", () => {
+            expect(all[0].completed).toBe(false);
+            markAsComplete(0);
+            expect(all[0].completed).toBe(true);
+        });
+        test("Should retrieve today items", () => {
+            expect(all.dueToday).toBe(all.dueDate);
+
+        });
+        test("Should retrieve overdue items.", () => {
+        
+            expect(all.dueDate).toBe(all.overdue);
+        
+        });
+        test("Should check due later items", () => {
+            expect(all.dueDate).toBe(all.dueLater);
+        
+        });    
+});
