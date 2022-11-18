71138d3716d9a9acfc9f95884f4b86b464389e0b
rwandantechy <niyinnocent2027@.com>
2022-11-18 17:22:30 +0530

    adding sequelize into my todo app

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,45 +1,48 @@
 /* eslint-disable no-undef */
-const todoList = require('../todo');
+const todoList = require("../todo");
 // eslint-disable-next-line no-unused-vars
-const { all,add,markAsComplete,overdue,dueToday,dueLater,toDisplayableList} = todoList();
+const {
+  all,
+  add,
+  markAsComplete,
+  overdue,
+  dueToday,
+  dueLater,
+  toDisplayableList,
+} = todoList();
 
-describe("Testing my todo app ", () => {
-    beforeAll(() => {
-        add({
-            title: "Test todo",
-            completed: false,
-            dueDate: new Date().toLocaleDateString("en-CA")
-        });
-    
-});
+describe("Testing my todo app:", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
 
-        test("Should add new todo ", () => {
-            const todoItemCount = all.length;
-            add({
-                title: "Test todo",
-                completed: false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            });
-    
-            expect(all.length).toBe(todoItemCount+1);
-        });
+  test("Should add new todo ", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
 
-        test("Should mark todo  as complete", () => {
-            expect(all[0].completed).toBe(false);
-            markAsComplete(0);
-            expect(all[0].completed).toBe(true);
-        });
-        test("Should retrieve today items", () => {
-            expect(all.dueToday).toBe(all.dueDate);
+    expect(all.length).toBe(todoItemCount + 1);
+  });
 
-        });
-        test("Should retrieve overdue items.", () => {
-        
-            expect(all.dueDate).toBe(all.overdue);
-        
-        });
-        test("Should check due later items", () => {
-            expect(all.dueDate).toBe(all.dueLater);
-        
-        });    
+  test("Should mark todo  as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("Should retrieve today items", () => {
+    expect(all.dueToday).toBe(all.dueDate);
+  });
+  test("Should retrieve overdue items.", () => {
+    expect(all.dueDate).toBe(all.overdue);
+  });
+  test("Should check due later items", () => {
+    expect(all.dueDate).toBe(all.dueLater);
+  });
 });
